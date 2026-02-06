d1afd32aa6abd23f7625555589c8e64a818c5890
Jake Poe <jakeapoe@gmail.com>
2026-02-06 18:06:05 -0500

    Commit reusable React components

---
+++ src/components/controls/button/button.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+import "./button.css";
+
+const Button = (props) => {
+    const classes = ["btn"];
+    if (props.className?.length) {
+        props.className.forEach((className) => {
+            classes.push(className);
+        })
+    }
+    return (
+        <>
+            {props.label && (
+                <label htmlFor={"button__" + props.id} className="button-label sr-only">
+                    {props.label}
+                </label>
+            )}
+            <button
+                id={"button__" + props.id}
+                onClick={props.onClickHandler}
+                className={classes.join(" ")}
+                type={props.type ? props.type : 'button'}
+                disabled={props?.disabled || false}>
+                <span>{props.text}</span>
+            </button>
+        </>
+    )
+}
+
+export default Button;
---
+++ src/components/layout/modal/modal.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from "react";
+import { connect } from "react-redux";
+
+import { setActiveImage } from "../../../state/actions/set-active-image";
+import Button from "../../controls/button/button";
+
+import './modal.css';
+
+const Modal = (props) => {
+    const [source, setSource] = useState('');
+    const closeModal = () => {
+        props.dispatch(setActiveImage(null))
+    }
+
+    useEffect(() => {
+        setSource(`${props.activeImage.source}/${props.activeImage.name}`);
+    }, [])
+
+    return (
+        <>
+            <div className="modal-backdrop" onClick={() => closeModal()}></div>
+            <div className="modal-content">
+                <Button id={`modal-close__${props.activeImage.id}`} type="close" onClickHandler={closeModal} />
+                <img className={"modal-image"} src={props.activeImage.url} alt="Modal" />
+                📷 Click <a href={source} target="_blank">here</a> for the full-resolution version.
+            </div>
+        </>
+    )
+}
+
+export default connect((state) => ({
+    activeImage: state.album.activeImage
+}))(Modal);
